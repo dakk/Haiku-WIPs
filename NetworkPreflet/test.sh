0220d750016f08652b0f1c90f304730aa0ff7961
Davide Gessa <dak.linux@gmail.com>
2011-12-17 13:40:08 +0000

    Update

---
+++ NetworkPreflet/test.sh
@@ -0,0 +1,5 @@
+rm -r /boot/home/haiku/src/preferences/network
+cp -r network /boot/home/haiku/src/preferences/
+cd /boot/home/haiku/src/preferences/network
+jam
+/boot/home/haiku/generated/objects/haiku/x86/release/preferences/network/Network
